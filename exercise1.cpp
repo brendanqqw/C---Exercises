8645181a38dabfacd971bda55f517608d18094d2
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-03-30 20:32:06 +0800

    exercise 1

--- exercise1.cpp
+++ exercise1.cpp
@@ -3,3 +3,17 @@ Sample output:
 The natural numbers are:
 1 2 4 5 6 7 8 9 10 */
 
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+    cout << "Find the first 10 natural numbers." << endl;
+    cout << "----------------------------------" << endl;
+    cout << "The natural numbers are:" << endl;
+
+    for(int i = 1; i <= 10; i++){
+        cout << i << " ";
+    }
+    return 0;
+}
